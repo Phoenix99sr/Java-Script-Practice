d7acc1989adca668684d50352f16996874396b00
Subashis Roy <113207463+Phoenix99sr@users.noreply.github.com>
2022-12-03 14:07:38 +0530

    Add files via upload

---
+++ JavaScript/file3.js
@@ -0,0 +1,15 @@
+// let x = "10";
+// let x = 10;
+let y = 10;
+let x = 3;
+
+console.log(x==y);
+console.log(x===y);
+console.log(typeof x);
+console.log(typeof y);
+// var z = y+++x;
+// var z = ++y + x;
+var z = y/x;
+console.log(z);
+console.log(y);
+

204cbb41d0446652c62ca3d6cccaad82eb77bc31
Subashis Roy <113207463+Phoenix99sr@users.noreply.github.com>
2022-12-05 14:00:04 +0530

    Delete file1.js

--- JavaScript/file1.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello World");
-document.getElementById("btn").onclick = function(){
-    alert("This is a Dialouge Box.");
-}
-var x = parseFloat(prompt("Give a number : "));
-var y = parseFloat(prompt("Give a number : "));
-var result = x + y;
-console.log("Yuor Given Numbers are :" + x + " & " + y);
-console.log("The Summation is :"+ result);

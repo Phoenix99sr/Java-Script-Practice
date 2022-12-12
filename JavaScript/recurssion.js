c8a71ab73a72e44d60c494e929e887a1791a6753
Subashis Roy <113207463+Phoenix99sr@users.noreply.github.com>
2022-12-12 16:08:11 +0530

    Add files via upload

---
+++ JavaScript/recurssion.js
@@ -0,0 +1,48 @@
+function commonFunction(method) {
+  let word = document.getElementById("input1").value;
+  let n = document.getElementById("input2").value;
+
+  switch (method) {
+    case "palindrome":
+      let stored = word;
+      if (word.split("").reverse().join("") === stored) {
+        document.getElementById("color").style.backgroundColor = "green";
+      } else {
+        document.getElementById("color").style.backgroundColor = "red";
+      }
+      break;
+    case "factorial":
+      let answer = 1;
+      if (n == 0 || n == 1) {
+        return (document.getElementById("output").value = answer);
+      } else {
+        for (var i = n; i >= 1; i--) {
+          answer = answer * i;
+        }
+        return (document.getElementById("output").value = answer);
+      }
+      break;
+  }
+}
+// function isPalindrome() {
+//   let word = document.getElementById("input1").value;
+//   let stored = word;
+//   if (word.split("").reverse().join("") === stored) {
+//     document.getElementById("color").style.backgroundColor = "green";
+//   } else {
+//     document.getElementById("color").style.backgroundColor = "red";
+//   }
+// }
+
+// function factorial() {
+//   let n = document.getElementById("input2").value;
+//   let answer = 1;
+//   if (n == 0 || n == 1) {
+//     return (document.getElementById("output").value = answer);
+//   } else {
+//     for (var i = n; i >= 1; i--) {
+//       answer = answer * i;
+//     }
+//     return (document.getElementById("output").value = answer);
+//   }
+// }

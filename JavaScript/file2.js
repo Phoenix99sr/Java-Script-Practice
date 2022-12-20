a6e5b0091c906043a71055717799195fddadf1aa
Subashis Roy <113207463+Phoenix99sr@users.noreply.github.com>
2022-12-20 19:34:55 +0530

    Delete file2.js

--- JavaScript/file2.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello World");
-var firstName = "Subashis";
-    lastName = "Roy";
-
-    console.log(firstName + " " + lastName);
-    console.log(typeof firstName );
-    console.log(firstName.toUpperCase() + " " + lastName.toUpperCase());
-    console.log("Name Code : " + firstName.slice(5,8) + lastName.slice(1,3));
-    var fullName = firstName+lastName;
